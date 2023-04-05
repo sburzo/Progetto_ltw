1082d168e0fbf2215e6d749b37ba5124ce4bec95
sburzo <andrea.sbuzi@hotmail.it>
2023-04-05 16:51:45 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-/*  ---------------------------------------------------
-    Template Name: Sona
-    Description: Sona Hotel Html Template
-    Author: Colorlib
-    Author URI: https://colorlib.com
-    Version: 1.0
-    Created: Colorlib
----------------------------------------------------------  */
 
 'use strict';
 
